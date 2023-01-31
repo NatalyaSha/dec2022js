30339eaec5c39eec2f341b91446de1ebfd37b328
NatalyaSha <natalyashalapalo@gmail.com>
2023-01-31 23:25:29 +0200

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,59 @@
+let aH = 'hello'
+console.log(aH);
+
+let bO = 'owu';
+console.log(bO);
+
+let cC = 'com';
+console.log(cC);
+
+let d ='ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f =10;
+console.log(f);
+
+let g =-999;
+console.log(g);
+
+let h =123;
+console.log(h);
+
+let i =3.14;
+console.log(i);
+
+let j =2.7;
+console.log(j);
+
+let k =16;
+console.log(k);
+
+let l =true;
+console.log(l);
+
+let m =false;
+console.log(m);
+
+let firstName ='Natalya';
+let middleName ='-';
+let lastName ='Lutsyshyn';
+console.log(`person ${firstName} ${middleName} ${lastName}`);
+
+let a =100;
+console.log(typeof a);
+
+let b ='100';
+console.log(typeof b);
+
+let c =true;
+console.log(typeof c);
+
+
+
+
+
+
+
